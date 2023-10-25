5d89d18a3e91e89b88d8bc9596e9afea6ac01030
vazkya <142280637+vazkya@users.noreply.github.com>
2023-10-25 13:08:52 +0700

    Add files via upload

---
+++ components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <header class="text-center bg-primary text-white p-lg-5">
+      <div class="row">
+        <div class="col-lg-1">
+          <img src="~/assets/img/logo.png" alt="logo">
+        </div>
+        <div class="col-lg">
+          <h1>Perpustakaan</h1>
+          <div>SMKN 4 Tasikmalaya</div>
+          <address>Jl.Depok, Sukamenak, Purbaratu 4619</address>
+        </div>
+      </div>
+    </header>
+
+    <Navbar />
+  </div>
+</template>
